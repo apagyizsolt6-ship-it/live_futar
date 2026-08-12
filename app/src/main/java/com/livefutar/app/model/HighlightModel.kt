b0cab5f1196110e26f319ed55bf846ac145756bf
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-08-12 20:20:32 +0200

    Add HighlightModel data class

---
+++ app/src/main/java/com/livefutar/app/model/HighlightModel.kt
@@ -0,0 +1,9 @@
+package com.livefutar.app.model
+
+data class HighlightModel(
+    val id: Int,
+    val title: String?,
+    val url: String?,
+    val embedUrl: String?,
+    val imgUrl: String?
+)
