b3d23a2f03e815634cb801dbf165c3ff1a55ac80
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-08-12 20:02:06 +0200

    Add ApiConstants for API configuration

---
+++ app/src/main/java/com/livefutar/app/data/ApiConstants.kt
@@ -0,0 +1,6 @@
+package com.livefutar.app.data
+
+object ApiConstants {
+    const val BASE_URL = "https://football-highlights-api.p.rapidapi.com/"
+    const val HOST_HEADER = "football-highlights-api.p.rapidapi.com"
+}
