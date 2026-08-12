3236be6d46fb6d00b69aaa24f87bd945b84986e7
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-08-12 20:08:02 +0200

    Update BASE_URL and HOST_HEADER in ApiConstants

--- app/src/main/java/com/livefutar/app/data/ApiConstants.kt
+++ app/src/main/java/com/livefutar/app/data/ApiConstants.kt
@@ -1,6 +1,5 @@
 package com.livefutar.app.data
 
 object ApiConstants {
-    const val BASE_URL = "https://football-highlights-api.p.rapidapi.com/"
-    const val HOST_HEADER = "football-highlights-api.p.rapidapi.com"
+    const val BASE_URL = "https://soccer.highlightly.net/"
 }
