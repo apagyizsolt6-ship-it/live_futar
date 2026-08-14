b5d36d3e07c14369eeb9f3ece16c0fb4dc7633c0
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-08-14 20:37:47 +0200

    Update ApiResponse.kt

--- app/src/main/java/com/livefutar/app/model/ApiResponse.kt
+++ app/src/main/java/com/livefutar/app/model/ApiResponse.kt
@@ -1,5 +1,12 @@
 package com.livefutar.app.model
 
 data class ApiResponse<T>(
-    val data: List<T>?
+    val data: List<T>?,
+    val pagination: Pagination? = null
+)
+
+data class Pagination(
+    val totalCount: Int? = null,
+    val offset: Int? = null,
+    val limit: Int? = null
 )
