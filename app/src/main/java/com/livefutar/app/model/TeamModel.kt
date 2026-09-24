5a67aadf660ae70dbb0a8f3780e85a3541bc2275
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-09-24 19:06:54 +0200

    TeamModel

--- app/src/main/java/com/livefutar/app/model/TeamModel.kt
+++ app/src/main/java/com/livefutar/app/model/TeamModel.kt
@@ -1,8 +1,14 @@
 package com.livefutar.app.model
 
+import com.livefutar.app.util.HungarianNames
+
 data class TeamModel(
     val id: Long,
     val name: String?,
     val logo: String?,
     val type: String?
-)
+) {
+    /** Magyar megjelenítendő név (ha van fordítás). */
+    val displayName: String
+        get() = HungarianNames.team(name)
+}
